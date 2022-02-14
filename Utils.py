c1ffb758d02121ff143c2dae53d3aecdb760baa0
yamyyack <jsjohnston98@gmail.com>
2022-02-14 12:35:02 -0500

    clean up and json creation

--- Utils.py
+++ Utils.py
@@ -1,20 +1,7 @@
 import math
 import random
 
-def ConvertFileToArray(file):
-    Values = []
-    Positions = []
 
-    for line in file:
-        if line[0] != "#":
-            array = line.split(":")
-            position = array[0][1:-2]
-            value = array[1][3:-3]
-            positionToArray = position.split("  ")
-            valueToArray = value.split(" ")
-            Positions.append(positionToArray)
-            Values.append(valueToArray)
-    return Values, Positions
 
 def getAbosolutePosition(arrayOfChoices, choicesPerPlayer):
     i = 0
@@ -26,11 +13,7 @@ def getAbosolutePosition(arrayOfChoices, choicesPerPlayer):
         i+=1
     return sum
 
-def createPlayers(classOfPlayer, playerChoices, Values):
-    players = []
-    for x in range(len(playerChoices)):
-        players.append(classOfPlayer(x, playerChoices, Values))
-    return players
+
 
 def randomChoice(playerChoices):
     choice = []
@@ -38,15 +21,4 @@ def randomChoice(playerChoices):
         choice.append(random.randint(1,x))
     return choice
 
-def iteration(players, currentChoice, Values, playerChoices, step):
-    tempChoice = []
-    for player in players:
-        tempChoice.append(player.getNextChoice(currentChoice.copy()))
-    print("this is the choice after the : {0}".format(step))
-    print(tempChoice)
-    print("\n")
-    i = 0
-    for player in players:
-        player.setCurrentOutcome(float(Values[getAbosolutePosition(tempChoice, playerChoices)][i]))
-        i += 1
-    return tempChoice
+
--- main.py
+++ main.py
@@ -1,21 +1,74 @@
 import math
 import os
+import json
 from datetime import datetime
 
 from AgentIBR import AgentIBR
 from Joueur import Joueur
 from Utils import *
 
-playerChoices = [3,3,3]
+
+def iteration(players, currentChoice, Values, playerChoices, step):
+    tempChoice = []
+    for player in players:
+        tempChoice.append(player.getNextChoice(currentChoice.copy()))
+    print("this is the choice after the : {0}".format(step))
+    print(tempChoice)
+    print("\n")
+    i = 0
+    for player in players:
+        player.setCurrentOutcome(float(Values[getAbosolutePosition(tempChoice, playerChoices)][i]))
+        i += 1
+    return tempChoice
+
+def createPlayers(classOfPlayer, playerChoices, Values):
+    players = []
+    for x in range(len(playerChoices)):
+        players.append(classOfPlayer(x, playerChoices, Values))
+    return players
+
+def ConvertFileToArray(file):
+    Values = []
+    Positions = []
+
+    for line in file:
+        if line[0] != "#":
+            array = line.split(":")
+            position = array[0][1:-2]
+            value = array[1][3:-3]
+            positionToArray = position.split("  ")
+            valueToArray = value.split(" ")
+            Positions.append(positionToArray)
+            Values.append(valueToArray)
+    return Values, Positions
+
+
+def loadJsonFile():
+    global playerChoices
+    configSettings = ""
+    f = open('config.json')
+    data = json.load(f)
+
+    configSettings += " -players " + str(len(data['number of choices per player'])) + " -actions "
+    for x in data['number of choices per player']:
+        configSettings += str(x) + " "
+    configSettings += " -min_payoff " + str(data['min payoff']) + " -max_payoff " + str(data['max payoff'])
+    if data['int payoff'] == True:
+        configSettings += " -int_payoffs -int_mult " + str(data['int payoff multiplier'])
+
+    playerChoices = data['number of choices per player']
+
+    f.close()
+    return configSettings
+
+playerChoices = []
 players = []
 
 
 if __name__ == '__main__':
     filename = datetime.today().strftime('%Y%m%d%H%M%S')
-    #os.system('cmd /c "java -jar gamut.jar -g BattleOfTheSexes -normalize -min_payoff 0 -max_payoff 150 -f games/{0}.game"'.format(filename))
     os.system(
-        'cmd /c "java -jar gamut.jar -g RandomGame -players 3 -normalize -min_payoff 0 -max_payoff 5 -int_payoffs -int_mult 1 -f BoS.game -actions 3 3 3 -f games/{0}.game"'.format(
-            filename))
+        'cmd /c "java -jar gamut.jar -g RandomGame -normalize {0} -f games/{1}.game"'.format(loadJsonFile(), filename))
     file = open("games/{0}.game".format(filename), "r")
 
     Values, Positions = ConvertFileToArray(file)
